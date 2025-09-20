849190160680c2b8350bd2e0087ab08e7cd7a376
AymaneELaamaj <aymaneamg300@gmail.com>
2025-09-20 16:56:36 +0200

    kidswith candels

--- Array&Strings/gcdOfStrings/Solution.java
+++ Array&Strings/gcdOfStrings/Solution.java
@@ -15,5 +15,6 @@ class Solution {
         if (b == 0) return a;
         return gcd(b, a % b);
     }
+    
 }
 
---
+++ Array&Strings/kidsWithCandies/Solution.java
@@ -0,0 +1,29 @@
+package kidsWithCandies;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> result = new ArrayList<>();
+        
+        int maxCandies = 0;
+        for (int candy : candies) {
+            if (candy > maxCandies) {
+                maxCandies = candy;
+            }
+        }
+        
+        for (int candy : candies) {
+            if (candy + extraCandies >= maxCandies) {
+                result.add(true);
+            } else {
+                result.add(false);
+            }
+        }
+        
+        return result;
+    }
+}
+
+
